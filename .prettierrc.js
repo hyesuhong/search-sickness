20372d2b8891532265065434492f6b7f9211338d
Hyesu <honghs95@gmail.com>
2023-09-05 22:08:57 +0900

    setting: lint, fotmat, git hook 세팅

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	singleQuote: true,
+	endOfLine: 'lf',
+	trailingComma: 'es5',
+	bracketSpacing: false,
+	jsxSingleQuote: true,
+	tabWidth: 2,
+	useTabs: true,
+	printWidth: 100,
+	proseWrap: 'always',
+	arrowParens: 'avoid',
+};
