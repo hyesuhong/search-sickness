34794a6a379bd250439aa23899ee43cfae1341ea
Hyesu <honghs95@gmail.com>
2023-09-07 00:16:39 +0900

    test: fetch 클래스로 데이터 요청할 때 cache 사용하는지 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import GlobalStyle from './styles/Global.styled';
 import {theme} from './styles/theme';
 import Search from './pages/Search';
 import CacheStore from './store/cache';
+import Api from './service/api';
 
 const EXPIRE_TIME = 1000 * 60;
 const cacheStorage = new CacheStore('test_1', EXPIRE_TIME);
 console.info(cacheStorage);
+
+const BASE_URL = 'http://localhost:4000/';
+const api = new Api(BASE_URL, cacheStorage);
+api
+	.fetch('sick?q=담')
+	.then(res => {
+		console.info(res);
+	})
+	.catch(console.error)
+	.finally(() => console.info('fin'));
 
 const App = () => {
 	return (
