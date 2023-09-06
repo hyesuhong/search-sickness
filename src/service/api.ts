0f84bbd771f16a86f35fba9263016db99c0e3789
Hyesu <honghs95@gmail.com>
2023-09-07 00:15:54 +0900

    feat: fetch 클래스 생성

---
+++ src/service/api.ts
@@ -0,0 +1,40 @@
+import type CacheStore from '../store/cache';
+
+export default class Api {
+	private BASE_URL: string;
+	private cacheStorage?: CacheStore;
+
+	constructor(baseUrl: string, cacheStorage?: CacheStore) {
+		this.BASE_URL = baseUrl;
+		this.cacheStorage = cacheStorage;
+	}
+
+	async fetch(endpoint: string) {
+		const targetUrl = `${this.BASE_URL}${endpoint}`;
+
+		if (!this.cacheStorage) {
+			return this.getApiData(targetUrl);
+		}
+
+		const cacheData = await this.getCache(targetUrl);
+		return cacheData ? cacheData : this.getApiData(targetUrl);
+	}
+
+	private async getCache(url: string) {
+		if (!this.cacheStorage) return undefined;
+
+		const cache = await this.cacheStorage.match(url);
+		return cache;
+	}
+
+	private async getApiData(url: string) {
+		const res = await fetch(url);
+
+		if (this.cacheStorage) {
+			const cloneRes = res.clone();
+			this.cacheStorage.put(url, cloneRes);
+		}
+
+		return res.json();
+	}
+}
