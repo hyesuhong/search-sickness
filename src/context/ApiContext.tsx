08f90e7a38639528d649265b164444c33ed0a5c8
Hyesu <honghs95@gmail.com>
2023-09-07 14:16:05 +0900

    feat: api 사용할 context 추가

---
+++ src/context/ApiContext.tsx
@@ -0,0 +1,47 @@
+import {ReactNode, createContext, useCallback, useContext, useMemo} from 'react';
+import Api from '../service/api';
+
+type Obj = {[key: string]: string};
+
+interface Props {
+	api: Api;
+	children?: ReactNode;
+}
+
+interface Value {
+	getData?: (url: string, querys?: Obj) => Promise<any>;
+}
+
+const ApiContext = createContext<Value>({});
+
+const ApiProvider = ({api, children}: Props) => {
+	const getData = useCallback(
+		async (url: string, querys?: Obj) => {
+			const queryStr = querys
+				? Object.keys(querys)
+						.map(query => `${query}=${querys[query]}`)
+						.join('&')
+				: '';
+			const endpoint = querys ? `${url}?${queryStr}` : url;
+
+			const response = await api.fetch(endpoint);
+			return response;
+		},
+		[api]
+	);
+
+	const contextValue = useMemo(
+		() => ({
+			getData,
+		}),
+		[getData]
+	);
+
+	return <ApiContext.Provider value={contextValue}>{children}</ApiContext.Provider>;
+};
+
+export const useApi = () => {
+	return useContext(ApiContext);
+};
+
+export default ApiProvider;
