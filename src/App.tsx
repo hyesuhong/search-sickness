e8df36efb6c534a7a1446f21ef53a37bf8bef21a
Hyesu <honghs95@gmail.com>
2023-09-07 14:27:02 +0900

    feat: apiContext 설정

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,22 @@ import {theme} from './styles/theme';
 import Search from './pages/Search';
 import CacheStore from './store/cache';
 import Api from './service/api';
+import ApiProvider from './context/ApiContext';
 
 const EXPIRE_TIME = 1000 * 60;
 const cacheStorage = new CacheStore('test_1', EXPIRE_TIME);
-console.info(cacheStorage);
 
 const BASE_URL = 'http://localhost:4000/';
 const api = new Api(BASE_URL, cacheStorage);
-api
-	.fetch('sick?q=담')
-	.then(res => {
-		console.info(res);
-	})
-	.catch(console.error)
-	.finally(() => console.info('fin'));
 
 const App = () => {
 	return (
 		<>
 			<ThemeProvider theme={theme}>
 				<GlobalStyle />
-				<Search />
+				<ApiProvider api={api}>
+					<Search />
+				</ApiProvider>
 			</ThemeProvider>
 		</>
 	);
