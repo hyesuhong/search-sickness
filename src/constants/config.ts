dedd936996b101867b039d5a8160f3266c973c91
Hyesu <honghs95@gmail.com>
2023-09-08 21:28:12 +0900

    setting: API 분리해, 로컬에 있는 관련 파일 수정

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 export const STORAGE_NAME = 'RECOMM_keyword';
 export const EXPIRE_TIME = 1000 * 60 * 60 * 12;
-export const BASE_URL = 'http://localhost:4000/';
+export const BASE_URL = 'https://json-server-sick.vercel.app/';
 export const DEBOUNCE_DELAY_TIME = 500;
 export const RESULT_MAX_LEN = 7;
